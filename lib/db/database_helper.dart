9031e0c0bb856742df2cddcc72e567ad2f9f82ca
Long Phung <longsydn1105@gmail.com>
2025-06-05 20:26:19 +0700

    update pubspec.yaml - hive sqflite path_provider path  db/database_helper.dart

---
+++ lib/db/database_helper.dart
@@ -0,0 +1,43 @@
+// lib/db/database_helper.dart
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final _dbName = 'chi_tieu.db';
+  static final _dbVersion = 1;
+
+  static Database? _database;
+
+  static Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  static Future<Database> _initDB() async {
+    final path = join(await getDatabasesPath(), _dbName);
+    return await openDatabase(path, version: _dbVersion, onCreate: _onCreate);
+  }
+
+  static Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE expenses (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        amount REAL,
+        category TEXT,
+        date TEXT,
+        note TEXT,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+      )
+    ''');
+  }
+}
