0b5b4bc6c401a9c89c7339747da597511c8123e1
anhbamoiditraive <ngchng789@gmail.com>
2025-06-07 22:46:32 +0700

    sửa trang welcome screen and main.dart

--- lib/Screens/welcome.dart
+++ lib/Screens/welcome.dart
@@ -1,10 +1,9 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'package:flutter_quanlythuchi/Screens/login.dart';
+import 'package:flutter_quanlythuchi/Screens/signup.dart';
 
 class MyWelcome extends StatelessWidget {
   const MyWelcome({Key? key}) : super(key: key);
@@ -20,20 +19,26 @@ class MyWelcome extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 40),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  SizedBox(),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  _welcomeLabel("Welcome", Color(0xff909090)),
-                  const SizedBox(
-                    height: 20,
-                  ),
+                  const SizedBox(height: 20),
+                  _welcomeLabel("Welcome", const Color(0xff909090)),
+                  const SizedBox(height: 20),
                   _logo(),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  _loginBtn((() => Navigator.pop(context))),
+                  const SizedBox(height: 20),
+                  _mainButton("Đăng nhập", () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => LoginPage()),
+                    );
+                  }),
+                  const SizedBox(height: 15),
+                  _mainButton("Đăng ký", () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => RegisterPage()),
+                    );
+                  }),
                 ],
               ),
             ),
@@ -42,51 +47,51 @@ class MyWelcome extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget _loginBtn(VoidCallback onPress) {
-  return Container(
-    width: 300,
-    height: 60,
-    decoration: const BoxDecoration(
-      color: Color(0xFF7C4DFF),
-      borderRadius: BorderRadius.all(Radius.circular(10)),
-    ),
-    child: TextButton(
-      onPressed: onPress,
-      child: Text(
-        "Logout",
-        style: GoogleFonts.josefinSans(
-          textStyle: const TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w800,
-            fontSize: 24,
+  Widget _mainButton(String text, VoidCallback onPress) {
+    return Container(
+      width: 300,
+      height: 60,
+      decoration: const BoxDecoration(
+        color: Color(0xFF7C4DFF),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: TextButton(
+        onPressed: onPress,
+        child: Text(
+          text,
+          style: GoogleFonts.josefinSans(
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w800,
+              fontSize: 24,
+            ),
           ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-Widget _welcomeLabel(String label, Color textColor) {
-  return Text(
-    label,
-    style: GoogleFonts.josefinSans(
-      textStyle: TextStyle(
-        color: textColor,
-        fontWeight: FontWeight.bold,
-        fontSize: 35,
+  Widget _welcomeLabel(String label, Color textColor) {
+    return Text(
+      label,
+      style: GoogleFonts.josefinSans(
+        textStyle: TextStyle(
+          color: textColor,
+          fontWeight: FontWeight.bold,
+          fontSize: 35,
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-Widget _logo() {
-  return Center(
-    child: SizedBox(
-      height: 300,
-      width: 300,
-      child: SvgPicture.asset("images/welcome.svg"),
-    ),
-  );
+  Widget _logo() {
+    return Center(
+      child: SizedBox(
+        height: 300,
+        width: 300,
+        child: SvgPicture.asset("images/welcome.svg"),
+      ),
+    );
+  }
 }
